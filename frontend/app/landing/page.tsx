0d24d67a17e63395158adb0c65ab6a055cdfc8b4
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-10 15:41:12 -0700

    installed supabase

--- frontend/app/landing/page.tsx
+++ frontend/app/landing/page.tsx
@@ -1,9 +1,18 @@
-const Landing = () => {
-  return(
-    <div>
-      <p>Hello World!</p>
-    </div>
-  );
-};
 
-export default Landing;
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
